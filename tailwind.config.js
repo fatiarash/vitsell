30a5cd5527721b7bb3a3239d4ca7e4e1226c197c
Your Name <you@example.com>
2023-02-18 19:23:24 +0330

    create project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
